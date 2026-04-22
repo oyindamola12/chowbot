3ce10b0d30cf41e5718652d1379de09735a559c3
oyindamola12 <mshittu234@gmail.com>
2026-04-22 23:59:26 +0100

    first commit

--- index.js
+++ index.js
@@ -955,6 +955,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
 
 const sessions = {};
